48dbd85f6926379d1a31b8f769493a3961fefee5
Anushka <anushkakohli2021@gmail.com>
2024-01-04 14:56:10 +0530

    customised token using open zeppelin

--- OpenZeppelin.sol
+++ OpenZeppelin.sol
@@ -9,8 +9,8 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract Block is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Block", "BLK") {
         _mint(msg.sender, initialSupply);
     }
 }
